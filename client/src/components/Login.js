5847f1d3be8a95e56fd6bf9b3c58995c557e78e5
c00kamunga <tsothalanti2@yahoo.com>
2020-05-15 17:17:27 -0700

    created login function within Login component that takes it axiosWithAuth post request

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,23 @@
 import React, { useState } from "react";
+import { axiosWithAuth } from '../axiosWithAuth';
 
 
-const Login = () => {
+const Login = (props) => {
+  const [credentials, setCredentials] = useState({})
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+
+  const login = e => {
+    e.preventDefault();
+    axiosWithAuth().post("/login", credentials)
+    .then(res => {
+      localStorage.setItem('token', res.data.payload);
+      props.history.push('/protected');
+    })
+  }
+
+
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
