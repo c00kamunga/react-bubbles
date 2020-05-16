b45dae1c8addac4ed334c9346a7c8c9f6814a4d7
c00kamunga <tsothalanti2@yahoo.com>
2020-05-15 18:19:17 -0700

    removed the useState that was imported in index.js and corrected some other typos

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,17 @@
 import React, { useState } from "react";
 import { Route, Link } from 'react-router-dom';
 import Login from "./components/Login";
+import PrivateRoute from './components/PrivateRoute';
+import BubblePage from './components/BubblePage';
 import "./styles.scss";
 
 function App() {
   return (
-    <Router>
       <div className="App">
+        <Link to="/bubblepage">Bubble Page</Link>
         <Route exact path="/" component={Login} />
-        {/* 
-
-        
-          Build a PrivateRoute component that will 
-          display BubblePage when you're authenticated 
-        */}
+        <PrivateRoute path="/bubblepage" component={BubblePage}/>
       </div>
-    </Router>
   );
 }
 
